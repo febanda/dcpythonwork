df14529ac4e653b55c073ead8f0cc92788ffef07
Francisco Banda <febanda91@gmail.com>
2018-03-03 15:32:00 +0000

    completed excercise

---
+++ excercise1.py
@@ -0,0 +1,27 @@
+In [73]: name = input('What is your name?')
+What is your name?Bob
+
+In [74]: print('Hello Bob!')
+Hello Bob!
+
+In [75]: print('Hello, Bob!')
+Hello, Bob!
+
+In [76]: name = input('WHAT IS YOUR NAME?')
+WHAT IS YOUR NAME?Bob
+
+In [77]: print("""HELLO, BOB!
+    ...: YOUR NAME HAS 3  LETTERS IN IT! AWESOME!""")
+HELLO, BOB!
+YOUR NAME HAS 3  LETTERS IN IT! AWESOME!
+
+In [78]: name = input('What is your name?')
+What is your name?Daniel
+
+In [79]: subject = input('What is your favorite subject?')
+What is your favorite subject?Science
+
+
+In [97]: print(name + 's' + ' favorite subject in school is ' + subject)
+Daniels favorite subject in school is Science
+
