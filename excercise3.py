7ba8aec4d0bcfd7757a6002f7cd49e84eb9b07c8
Francisco Banda <febanda91@gmail.com>
2018-03-05 02:37:55 +0000

    pythonpart1

--- excercise3.py
+++ excercise3.py
@@ -1,1 +1,42 @@
-print("hello")
+import random
+
+def play_again():
+   play_again = (input("Do you want to play again (Y or N)? ")).upper()
+   if play_again == "Y":
+       guess_number()
+   else:
+       print("Bye!")
+
+def guess_number():
+   secret_number = random.randint(1, 10)
+   print("I am thinking of a number between 1 and 10.")
+   print("You have 5 guesses left.")
+   count = 4
+
+   while count >= 0:
+       guess = int(input("What's the number? "))
+
+       if guess == secret_number:
+           print("Yes! You win!")
+           play_again()
+           break
+
+       elif count == 0:
+           print("You ran out of guesses! The secret number was {}".format(secret_number))
+           play_again()
+           break
+
+       elif guess > secret_number:
+           print("{} is too high.".format(guess))
+           print("You have {} guesses left.".format(count))
+
+       else:
+           print("{} is too low.".format(guess))
+           print("You have {} guesses left.".format(count))
+
+       count -= 1
+
+guess_number()
+
+       
+       
